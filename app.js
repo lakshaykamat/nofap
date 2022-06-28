977f754294714884d0ecfa6719851723c0ad430d
Lakshay Kamat <lakshaykamat2048@gmail.com>
2022-06-28 22:56:37 +0530

    all files added

---
+++ app.js
@@ -0,0 +1,25 @@
+const dayEl = document.getElementById('days')
+const hourEl = document.getElementById('hours')
+const minEl = document.getElementById('mins')
+const secEl = document.getElementById('seconds')
+function start(){
+    let input = document.getElementById('input').value//garbing the value
+    let inputMilli = new Date(input).getTime()//converting to millisecond(user time)
+    let currentDate = new Date().getTime()//current time in milliseconds
+    let gap = currentDate - inputMilli //subtracting to get the difference
+    
+    const second = 1000//1 second has 1000 milliseconds
+    const minute = second * 60 // coverting minute to milliseconds
+    const hour = minute * 60 //converting hour to milliseconds
+    const day = hour * 24 //converting day to milliseconds
+
+    let textDay = Math.floor(gap / day) //dividing to get the day passed
+    let textHour = Math.floor((gap % day )/ hour)
+    let textMin = Math.floor((gap % hour )/ minute)
+    let textSecond = Math.floor((gap % minute )/ second)
+    dayEl.textContent = textDay
+    hourEl.textContent = textHour
+    minEl.textContent = textMin
+    secEl.textContent = textSecond
+}
+setInterval(start,1000)
